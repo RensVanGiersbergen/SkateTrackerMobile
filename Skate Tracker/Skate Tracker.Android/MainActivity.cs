23a36eb51f8da0d33daa1f79975ed46bd1e818dc
Rens van Giersbergen <461941@student.fontys.nl>
2022-04-10 20:04:38 +0200

    Add project files.

---
+++ Skate Tracker/Skate Tracker.Android/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android;
+
+namespace Skate_Tracker.Droid
+{
+    [Activity(Label = "Skate_Tracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        const int RequestLocationID = 0;
+
+        readonly string[] LocationPermissions =
+        {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation
+        };
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            if((int)Build.VERSION.SdkInt >= 23)
+            {
+                if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted)
+                {
+                    RequestPermissions(LocationPermissions, RequestLocationID);
+                }
+                else
+                {
+                    //permissions already granted
+                }
+            }
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
+            Window.SetStatusBarColor(Android.Graphics.Color.Rgb(255, 119, 0));
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Skate Tracker/Skate Tracker/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace Skate_Tracker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Geocoder geoCoder = new Geocoder();
+            Button StartStopJourney = new Button()
+            {
+                Text = "Start",
+                TextColor = Color.Snow,
+                BackgroundColor = Color.FromHex("#ff7700"),
+                CornerRadius = 5,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.End
+            };
+
+            Map map = new Map()
+            {
+                IsShowingUser = true
+            };
+        
+            Position currentPosition = new Position(51.565429, 5.375261);
+
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(currentPosition, Distance.FromMeters(100)));
+            Polyline route = new Polyline()
+            {
+                StrokeColor = Color.FromHex("#ff7700"),
+                StrokeWidth = 5,
+                Geopath =
+                {
+                    currentPosition,
+                    new Position(52, 5.30),
+                    new Position(53, 6)
+                }
+            };
+            map.MapElements.Add(route);
+
+            Content = new StackLayout()
+            {
+                Children =
+                {
+                    map,
+                    StartStopJourney
+                }
+            };
+        }
+    }
+}
